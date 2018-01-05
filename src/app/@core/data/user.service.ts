e9206405ac7f05311934c2b6e52d500ba813460b
santhosh17s <santhoshloud@gmail.com>
2018-01-05 15:57:45 +0530

    Login user authentication

---
+++ src/app/@core/data/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { SystemUser } from './systemuser';
+
+@Injectable()
+export class UserService {
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<SystemUser[]>('/api/users');
+    }
+
+    getById(id: number) {
+        return this.http.get('/api/users/' + id);
+    }
+
+    create(user: SystemUser) {
+        return this.http.post('/api/users', user);
+    }
+
+    update(user: SystemUser) {
+        return this.http.put('/api/users/' + user.id, user);
+    }
+
+    delete(id: number) {
+        return this.http.delete('/api/users/' + id);
+    }
+}
